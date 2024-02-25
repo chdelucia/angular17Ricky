0cd424ff465638376b92df77240618d3f871262a
Christian Heredia <cheredianaranjo.it@hbplat.com>
2024-02-25 14:42:43 +0100

    update unit test

--- src/app/characters/feautre-characters/pages/similar-character/similar-character.component.spec.ts
+++ src/app/characters/feautre-characters/pages/similar-character/similar-character.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SimilarCharacterComponent } from './similar-character.component';
+import { CharacterService } from '@characters-data/services';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
+
+const characterServiceStub = {
+  searchCharacters: () => of({}),
+  getDetails: () => of({ name: 'test' }),
+};
 
 describe('SimilarCharacterComponent', () => {
   let component: SimilarCharacterComponent;
@@ -9,6 +17,10 @@ describe('SimilarCharacterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SimilarCharacterComponent],
+      providers: [
+        { provide: CharacterService, useValue: characterServiceStub },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SimilarCharacterComponent);
